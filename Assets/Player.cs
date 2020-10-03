e66848f49d5eb6bcab2849940ac2dab601170daf
Tac Tacelosky <tacman@gmail.com>
2020-10-03 12:59:58 -0400

    add score and tweak speed

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,8 +11,10 @@ public class Player : MonoBehaviour
 
     private float _movement = 0f;
 
-    public Text scoreText;
+    private int _score = 0;
 
+    public Text scoreText;
+    
     void Update()
     {
         // no dampening with raw.
@@ -21,13 +23,20 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        scoreText.text = _movement.ToString("0");
         // since 0 is the center of the screen, we can rotate the player around the center.  .forward is the z axis.
         transform.RotateAround(Vector3.zero, Vector3.forward, _movement * Time.fixedDeltaTime * -moveSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        if (collision.name == "Opening")
+        {
+            _score += 1;
+            scoreText.text = _score.ToString("D");
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
