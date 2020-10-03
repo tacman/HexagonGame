81252a8fd65beaa622b55af5c5841ee5880d4b50
Tac Tacelosky <tacman@gmail.com>
2020-10-03 10:32:03 -0400

    initial version

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 600f;
+
+    private float _movement = 0f;
+
+    public Text scoreText;
+
+    void Update()
+    {
+        // no dampening with raw.
+        _movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        scoreText.text = _movement.ToString("0");
+        // since 0 is the center of the screen, we can rotate the player around the center.  .forward is the z axis.
+        transform.RotateAround(Vector3.zero, Vector3.forward, _movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Hexagon game 
+
+From https://www.youtube.com/watch?v=p8MzsDBI5EI
+
+Use the left/right arrow keys to spin around and avoid the hexagons.
+
