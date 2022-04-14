b074527e151067a57696c845df548f2721a85f4e
Tac Tacelosky <tacman@gmail.com>
2022-04-14 12:15:33 -0400

    bump to 2021.3f1, use new Input System

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,21 +4,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 600f;
 
     private float _movement = 0f;
+    private Vector2 _movementVector;
 
     private int _score = 0;
 
     public Text scoreText;
+
+    public void OnMove(InputValue input)
+    {
+        Vector2 inputVec = input.Get<Vector2>();
+        Debug.Log(inputVec.ToString());
+
+        _movementVector = new Vector3(inputVec.x, 0, inputVec.y);
+        _movement = _movementVector.x;
+    }
     
     void Update()
     {
+        
         // no dampening with raw.
-        _movement = Input.GetAxisRaw("Horizontal");
+        // _movement = Input.GetAxisRaw("Horizontal");
     }
 
     private void FixedUpdate()
